0e59da8432e43a11564815ace9d1f9345b48399b
J. R. Swab <jrswab@protonmail.com>
2018-06-24 17:10:14 +0000

    changed html title

--- hash.php
+++ hash.php
@@ -2,7 +2,7 @@
 <html>
 	<head>
 		<meta charset="UTF-8">
-		<title>Welcome</title>
+		<title>Your Hash!</title>
 		<link rel="stylesheet" type="text/css" href="style.css?v=<?=time();?>">
 		<link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.1.1/css/bootstrap.min.css" integrity="sha384-WskhaSGFgHYWDcbwN70/dfYBj47jz9qbsMId/iRN3ewGhXQFZCSftd1LZCfmhktB" crossorigin="anonymous">
 	</head>	
