a38387e81f00a71e6cea5d8a798146de6c6a7003
Jaron <jrswab@gmail.com>
2018-06-29 11:39:52 -0400

    base for user profile photo upload

---
+++ execs/proPhoUp.php
@@ -0,0 +1,43 @@
+<?php include 'config/topMem.php';
+
+if (isset($_POST['submit'])) {
+	$file = $_FILES['file']; // define file
+
+	$fileName = $_FILES['file']['name']; // grab the file name
+	$fileTmpName = $_FILES['file']['tmp_name']; // define file temp name
+	$fileSize = $_FILES['file']['size']; // grab the file size
+	$fileError = $_FILES['file']['error']; // define error code
+	$fileType = $_FILES['file']['type']; // grab the file type
+
+	// separate the file extension from the file name
+	$fileExt = explode('.', $fileName);
+	// convert the extension to lower case
+	$fileActualExt = strtolower(end($fileExt));
+
+	// allowed file extensions
+	$allowed = array('jpg', 'jpeg', 'png');
+
+	if (in_array($fileActualExt, $allowed)) { // check if file extension is allowed
+		if ($fileError === 0) { // check for no error codes
+			if ($fileSize < 2202010) { // make sure file size is less than 500MB
+				// give thu upload a unique name
+				$fileNameNew = $_SESSION['username'].".".$fileActualExt;
+				// define file upload end location
+				$fileDestination = '../uploads/profiles/'.$fileNameNew;
+				// move the file
+				move_uploaded_file($fileTmpName, $fileDestination);
+				// return user to settings.php
+				header['Location: ../settings.php'];
+			} else {
+				echo "Your file is too big. For best results please keep your file under 250MB.";
+			}
+		} else {
+			echo "There was an error during uploading. Please try again.";
+		}
+	} else {
+		echo "Sorry, the ".$fileActualExt." file type is not supported.";
+	}
+}
+
+include 'config/bottom.html';
+
--- settings.php
+++ settings.php
@@ -2,6 +2,19 @@
 			<h2>Settings</h2>
 			<h4></h4>
 		</div>
+		<h3>Upload Profile Image:</h3>
+		<p>Max allowed file size is 2MB</p>
+		<form id="profilePhoto" enctype="multipart/form-data" action="execs/proPhoUp.php">
+			<input class="form-input" type="file" name="proPho" />
+			<button id="proPhoClick" onclick="pgShow()" class="btn btn-success" name="submit" type="submit">Upload Photo</button>
+			<br /><br />
+			<div id="bar" style="display:none">
+					<div class="progress">
+						<div class="progress-bar progress-bar-striped progress-bar-animated" role="progressbar" aria-valuenow="100" aria-valuemin="0" aria-valuemax="100" style="width: 100%"></div>
+					</div>
+					<br>
+				</div>
+
 		<br>
 		<button class="btn btn-danger" onclick="pop()">Delete Account</button>
 		<br /><br />
@@ -27,5 +40,11 @@
 	function pop(){
 		document.getElementById("delAlert").style = "display: block";
 	}
+
+	function pgShow() {
+		var bar = document.getElementById("bar");
+		bar.style.display = "block";
+	}
+
 </script>
 <?php include 'config/bottom.html'; ?>
