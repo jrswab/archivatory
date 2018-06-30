64d20c66aec7d9a3409f8828cb6dd7b79ecd7d86
Jaron <jrswab@gmail.com>
2018-06-30 15:10:44 -0400

    added profile photo deletion to user delete opiton

--- execs/delUser.php
+++ execs/delUser.php
@@ -16,21 +16,32 @@ if (isset($_POST['delAccount'])){
 	$runDelUp = mysqli_query($link, $sqlDelUp);
 	$runDelUser = mysqli_query($link, $sqlDelUser);
 
+	$getProPho = shell_exec('ls uploads/profiles/ | grep '.$user;
+	$runDelProPho = shell_exec('rm uploads/profiles/'.$getProPho;
+
 	if ($runDelUp) {
 		if ($runDelUser) {
-			header("Location: index.php");
+			if ($runDelProPho){
+				header("Location: index.php");
+			} else {
+				echo 'Could not delete '.$getProPho.;
+				echo '<br /><br /> Please take a screen shot and send it to the 
+				#support thread on our <a href="https://discord.gg/PVNKWDx"> 
+				Discord chat</a>';
+			}
 		} else {
-			echo "Could not delete account. <br />";
+			echo 'Could not delete account. <br />';
 			echo $link->error;
-			echo "<br><br>Please copy this page or take a screen shot and send it to the 
-			#support thread on our <a href='https://discord.gg/PVNKWDx'> 
-			Discord chat</a>";
+			echo '<br /><br /> Please take a screen shot and send it to the 
+			#support thread on our <a href="https://discord.gg/PVNKWDx"> 
+			Discord chat</a>';
 		}
 	} else {
 		echo "Could not delete user content table. <br />";
 		echo $link->error;
-		echo "<br><br>Please copy this page or take a screen shot and send it to the 
-			#support thread on our <a href='https://discord.gg/PVNKWDx'>Discord chat</a>";
+		echo '<br /><br /> Please take a screen shot and send it to the 
+		#support thread on our <a href="https://discord.gg/PVNKWDx"> 
+		Discord chat</a>';
 	}
 }
 
