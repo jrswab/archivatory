28e64c1e3e20b7365141b512664db930c84d7bfa
Jaron <jrswab@gmail.com>
2018-07-13 10:04:43 -0400

    basic RSS feed finalized

--- feeds.php
+++ feeds.php
@@ -1,6 +1,11 @@
 <?php
 	include 'config/uploadDBconfig.php';
 
+$dir = '';
+while (!glob($dir.'hash.php')) {
+	$dir .= '../';
+}
+
 	// tell php what type of content follows
 	header("Content-Type: application/rss+xml; charset=ISO-8859-1");
 
@@ -10,20 +15,29 @@
 	$endURL = end($URIArray);
 	$user = prev($URIArray);
 
+	// Get the full path to user profile photo
+	$proPho = shell_exec('ls '.$dir.'uploads/profiles/ | grep '.$user);
+
 	// top of rss feed
-	$rssfeed = '<?xml version="1.0" encoding="UTF-8"?>';
-	$rssfeed .= '<rss xmlns:dc="http://purl.org/dc/elements/1.1/" 
+	$rssfeed = '<?xml version="1.0" encoding="UTF-8"?>
+	<rss xmlns:dc="http://purl.org/dc/elements/1.1/" 
 		xmlns:content="http://purl.org/rss/1.0/modules/content/" 
 		xmlns:atom="http://www.w3.org/2005/Atom" version="2.0" 
-		xmlns:cc="http://cyber.law.harvard.edu/rss/creativeCommonsRssModule.html">';
-	$rssfeed .= '<channel>';
+		xmlns:cc="http://cyber.law.harvard.edu/rss/creativeCommonsRssModule.html">
+		<channel>';
 
 	// unchanging rss info
-	$rssfeed .= '<title>'.$user.'\'s Archivatory Feed</title>';
-	$rssfeed .= '<link>https://archivatory.com/u/'.$user.'/</title>';
-	$rssfeed .= '<description>This is the Archivatory RSS feed for '.$user.'</description>';
-	$rssfeed .= '<language>en-us</language>';
-	$rssfeed .= '<copyright>Copyright (C) 'date("Y")' '.$user.'</copyright>';
+	$rssfeed .= '
+			<title>'.$user.'\'s Archivatory Feed</title>
+			<link>'.$dir.'u/'.$user.'</link>
+			<description>This is the Archivatory RSS feed for '.$user.'</description>
+			<image>
+				<url>'.$dir.'uploads/profiles/'.$proPho.'</url>
+				<title>'.$user.'\'s Archivatory Feed</title>
+				<link>https://archivatory.com/u/'.$user.'</link>
+			</image>
+			<language>en-us</language>
+			<copyright>Copyright (C) '.date("Y").' '.$user.'</copyright>';
 
 	// query user data
 	$sql = "SELECT * FROM ".$user." WHERE playlist=1 ORDER BY date DESC;";
@@ -35,14 +49,16 @@
 		while ($row = mysqli_fetch_assoc($result)) { 
 			$rssfeed .= '
 			<item>
+				<title>'.$row["file_name"].'</title>
 				<link>https://ipfs.io/ipfs/'.$row["hash"].'</link>
 				<pubDate>'.$row["date"].'</pubDate>
 			</item>';
 		}
 	}
 
-$rssfeed .= '</channel>';
-$rssfeed .= '</rss>';
+	$rssfeed .= '
+		</channel>
+	</rss>';
 
 echo $rssfeed;
 ?>
